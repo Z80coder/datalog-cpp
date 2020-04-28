152a1e8985da5b7fb7dc10a4a7e72e3316dfc5f4
Ian Wright <wrighti@acm.org>
2020-04-28 15:22:57 +0100

    complete the tests

--- tests/variable_test.cpp
+++ tests/variable_test.cpp
@@ -3,21 +3,47 @@
 
 using namespace datalog;
 
-bool freeVariableTest() {
+bool freeVariableTest()
+{
     Variable<int> intVar;
     return !intVar.isBound();
 }
 
-bool boundVariableTest() {
+bool boundVariableTest()
+{
     Variable<int> intVar;
     intVar.bind(0);
     return intVar.isBound();
 }
 
-TEST_CASE( "An new variable is unbound", "[variable]" ) {
-    REQUIRE( freeVariableTest() == true );
+bool bindUnbindTest()
+{
+    Variable<int> intVar;
+    intVar.bind(0);
+    intVar.unbind();
+    return !intVar.isBound();
 }
 
-TEST_CASE( "A variable with a value is bound", "[variable]" ) {
-    REQUIRE( boundVariableTest() == true );
+bool storesValueTest()
+{
+    Variable<int> intVar;
+    const int value = 100;
+    intVar.bind(value);
+    return intVar.isBound() and intVar.value() == value;
+}
+
+bool absentValueTest()
+{
+    Variable<int> intVar;
+    const auto &val = intVar.value();
+    return true;
+}
+
+TEST_CASE("variable binding", "[variable]")
+{
+    REQUIRE(freeVariableTest());
+    REQUIRE(boundVariableTest());
+    REQUIRE(bindUnbindTest());
+    REQUIRE(bindUnbindTest());
+    REQUIRE_THROWS_AS(absentValueTest(), std::bad_optional_access);
 }
